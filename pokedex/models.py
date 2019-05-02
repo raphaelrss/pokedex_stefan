15e1bda5a9adbcbabdcc2c345c14f15ef064e0a6
Raphael Rodrigues <raphaxd@outlook.com>
2019-05-02 20:47:50 -0300

    Classe Pokemon criada

--- pokedex/models.py
+++ pokedex/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+class Pokemon(models.Model):
+    imagem = models.FileField(upload_to='fotos/')
+    nome = models.CharField(max_length=120)
+    pontos_de_vida = models.PositiveIntegerField(verbose_name= "Vida")
+    pontos_de_ataque = models.PositiveIntegerField(verbose_name= "Ataque")
+    pontos_de_defesa = models.PositiveIntegerField(verbose_name= "Defesa")
+    pontos_de_velocidade = models.PositiveIntegerField(verbose_name= "Velocidade")
+    ataque_especial = models.PositiveIntegerField(verbose_name= "Ataque Especial")
+    defesa_especial = models.PositiveIntegerField(verbose_name= "Defesa Especial")
+    peso = models.FloatField(verbose_name= "Peso")
+
+    def __str__(self):
+        return self.nome
