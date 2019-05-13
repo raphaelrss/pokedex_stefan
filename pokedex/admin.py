f4a72bd4e03bbfcac46fc85d8b151bfe7ee66aa3
Raphael Rodrigues <raphaxd@outlook.com>
2019-05-13 19:39:42 -0300

    classe tipo criada mas o projeto bugou

--- pokedex/admin.py
+++ pokedex/admin.py
@@ -3,3 +3,5 @@ from .models import *
 
 # Register your models here.
 admin.site.register(Pokemon)
+admin.site.register(Habilidade)
+admin.site.register(Tipo)
--- pokedex/models.py
+++ pokedex/models.py
@@ -2,16 +2,40 @@ from django.db import models
 
 # Create your models here.
 
-class Pokemon(models.Model):
-    imagem = models.FileField(upload_to='fotos/')
-    nome = models.CharField(max_length=120)
-    pontos_de_vida = models.PositiveIntegerField(verbose_name= "Vida")
-    pontos_de_ataque = models.PositiveIntegerField(verbose_name= "Ataque")
-    pontos_de_defesa = models.PositiveIntegerField(verbose_name= "Defesa")
-    pontos_de_velocidade = models.PositiveIntegerField(verbose_name= "Velocidade")
-    ataque_especial = models.PositiveIntegerField(verbose_name= "Ataque Especial")
-    defesa_especial = models.PositiveIntegerField(verbose_name= "Defesa Especial")
-    peso = models.FloatField(verbose_name= "Peso")
+
+class Habilidade(models.Model):
+    nome = models.CharField(max_length= 40, verbose_name= "Nome")
+    descricao_habilidade = models.CharField(max_length=200, verbose_name="Descrição")
 
     def __str__(self):
         return self.nome
+
+
+class Tipo(models.Model):
+    nome = models.CharField(max_length=50, verbose_name="Nome")
+    descricao_tipo = models.CharField(max_length=200, verbose_name="Descrição")
+
+    def __str__(self):
+        return self.nome
+
+    def __int__(self):
+        return self.id
+
+
+class Pokemon(models.Model):
+    imagem = models.FileField(upload_to='fotos/')
+    nome = models.CharField(max_length=120)
+    pontos_de_vida = models.PositiveIntegerField(verbose_name="Vida")
+    pontos_de_ataque = models.PositiveIntegerField(verbose_name="Ataque")
+    pontos_de_defesa = models.PositiveIntegerField(verbose_name="Defesa")
+    pontos_de_velocidade = models.PositiveIntegerField(verbose_name="Velocidade")
+    ataque_especial = models.PositiveIntegerField(verbose_name="Ataque Especial")
+    defesa_especial = models.PositiveIntegerField(verbose_name="Defesa Especial")
+    fk_habilidade = models.ManyToManyField(Habilidade)
+    fk_tipo = models.ManyToManyField(Tipo)
+    peso = models.FloatField(verbose_name="Peso")
+
+    def __str__(self):
+        return self.nome
+
+
