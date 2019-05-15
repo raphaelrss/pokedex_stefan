a11df00ec7304af686c27f6b2b560db3a7d460e5
raphaelrss <raphaxd@outlook.com>
2019-05-15 18:47:40 -0300

    view mostrar pokemons criada, basico.html criado

--- pokedex/urls.py
+++ pokedex/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path
+from .views import *
+from django import views
 
 urlpatterns = [
-
+    path('', views.mostrarPokemons, name='mostrar_pokemons')
 ]
--- pokedex/views.py
+++ pokedex/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
+
+def mostrarPokemons(request):
+    pokemons = Pokemon.objects.all()
+
+    contexto = {
+        "todos_pokemons": pokemons,
+    }
+    return render(request, 'basico.html', contexto)
