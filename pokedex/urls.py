3b48db2e52903e14c02c29691edaba908572ce1a
raphaelrss <raphaxd@outlook.com>
2019-05-22 21:46:44 -0300

    Card do pokemon criado e fazendo link com a lista.

--- pokedex/urls.py
+++ pokedex/urls.py
@@ -2,7 +2,9 @@ from django.contrib import admin
 from django.urls import path
 from .views import *
 
+app_name = "pokedex"
 
 urlpatterns = [
-    path("listar/", mostrarPokemons, name="mostrar_pokemons")
-]
+    path("listar/", listarPokemons, name="mostrar_pokemons"),
+    path("pokemon/<int:idpokemon>/", umPokemon, name="um_pokemon")
+    ]
--- pokedex/views.py
+++ pokedex/views.py
@@ -3,10 +3,19 @@ from .models import *
 
 # Create your views here.
 
-def mostrarPokemons(request):
+
+def listarPokemons(request):
     pokemons = Pokemon.objects.all()
 
     contexto = {
         "todos_pokemons": pokemons,
+    }
+    return render(request, 'listar.html', contexto)
+
+
+def umPokemon(request, idpokemon=None):
+    pokemon = Pokemon.objects.get(id=idpokemon)
+    contexto = {
+        "pokemon": pokemon,
     }
     return render(request, 'pokemon.html', contexto)
