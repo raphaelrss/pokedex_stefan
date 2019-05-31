83a525e278606d422a1797027fbd3448581590ae
Artxzy <sythenn@gmail.com>
2019-05-31 16:23:15 -0300

    pasta static criada e com o bg funcionando. obs. colocar a imagem que está no pc do curso

--- trabalho/settings.py
+++ trabalho/settings.py
@@ -114,7 +114,10 @@ USE_L10N = True
 
 USE_TZ = True
 
-
+STATICFILES_DIRS = [
+    # ...
+    ("downloads", "/opt/webfiles/stats"),
+]
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
