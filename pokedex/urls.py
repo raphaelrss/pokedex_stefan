f9d9383f070a099bbde7307532d6e95a34dce394
Artxzy <sythenn@gmail.com>
2019-06-03 11:49:30 -0300

    sem a ordenação, esperando a aplicação do data tables

--- pokedex/urls.py
+++ pokedex/urls.py
@@ -6,6 +6,5 @@ app_name = "pokedex"
 
 urlpatterns = [
     path("listar/", listarPokemons, name="mostrar_pokemons"),
-    path("listar/<str:ordenacao>/", listarPokemons, name="mostrar_pokemons"),
     path("pokemon/<int:idpokemon>/", umPokemon, name="um_pokemon")
     ]
